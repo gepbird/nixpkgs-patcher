cd70de260ed3f690040f0cf1aa6983fc20ce1146
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-02 17:24:37 +0200

    test: add a passing test for file-rename-fetchpatch, add failing test as comments

--- tests/file-rename-fetchpatch/flake.nix
+++ tests/file-rename-fetchpatch/flake.nix
@@ -6,7 +6,15 @@
 
   outputs =
     inputs: with inputs; {
-      nixosConfigurations.patched = nixpkgs-patcher.lib.nixosSystem {
+      nixosConfigurations.unpatched = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+        specialArgs = inputs;
+      };
+
+      nixosConfigurations.patchedFails = nixpkgs-patcher.lib.nixosSystem {
         modules = [
           ./configuration.nix
           ./hardware-configuration.nix
@@ -24,26 +32,42 @@
         ];
       };
 
-      nixosConfigurations.unpatched = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.patchedWorks = nixpkgs-patcher.lib.nixosSystem {
         modules = [
           ./configuration.nix
           ./hardware-configuration.nix
         ];
         specialArgs = inputs;
+        nixpkgsPatcher.patches = pkgs: [
+          (pkgs.fetchurl {
+            name = "msmtp-build-fix-1-8-30.diff";
+            url = "https://github.com/NixOS/nixpkgs/pull/425312.diff";
+            hash = "sha256-Laf37gN6jmU2FCCKRHgsiwceMmM5aPSHVi56bPfVB8g=";
+          })
+        ];
       };
 
       checks.x86_64-linux.tests =
         let
-          inherit (self.nixosConfigurations) patched unpatched;
+          inherit (self.nixosConfigurations) unpatched patchedFails patchedWorks;
           lib = import ../lib.nix { inherit nixpkgs; };
         in
         lib.runTests {
+          # this is failing, because msmtp doesn't build
+          #testUnpatchedSystemBuilds = lib.testNixosConfigurationBuilds unpatched;
+          # this is failing, because fetchpatch2 ignores renames, it should be asserted
+          #testPatchedFailsSystemBuilds = lib.testNixosConfigurationBuilds patchedFails;
+          testPatchedWorksSystemBuilds = lib.testNixosConfigurationBuilds patchedWorks;
           testUnpatchedPackageVersion = {
             expr = unpatched.pkgs.msmtp.version;
             expected = "1.8.26";
           };
-          testPatchedPackageVersion = {
-            expr = patched.pkgs.msmtp.version;
+          testPatchedFailsPackageVersion = {
+            expr = patchedFails.pkgs.msmtp.version;
+            expected = "1.8.30";
+          };
+          testPatchedWorksPackageVersion = {
+            expr = patchedWorks.pkgs.msmtp.version;
             expected = "1.8.30";
           };
         };
