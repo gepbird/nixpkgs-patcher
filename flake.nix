727d72c5b220659cafb2334bb1069949b2396fa2
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-18 22:18:05 +0200

    refactor: move nixosSystem function to the top let scope

--- flake.nix
+++ flake.nix
@@ -81,6 +81,122 @@
             # breakpontHook message gets inserted here
           '';
         };
+
+      nixosSystem =
+        args:
+        let
+          metadataModule = {
+            # this should be using `nixpkgsPatched` rather than `nixpkgs`
+            # but that will slow down every command that tries to look up the nixpkgs flake
+            # with the message 'copying "/nix/store/AAA..-patched" to the store'
+            config.nixpkgs.flake.source = toString nixpkgs;
+
+            config.system.nixos.versionSuffix = ".${nixpkgsVersion { inherit nixpkgs patches; }}";
+
+            config.system.nixos.revision = nixpkgs.rev or "dirty";
+          };
+
+          nixpkgsPatcherNixosModule =
+            { lib, ... }:
+
+            let
+              inherit (lib)
+                mkOption
+                mkEnableOption
+                literalExpression
+                types
+                ;
+            in
+            {
+              options.nixpkgs-patcher = {
+                enable = mkEnableOption "nixpkgs-patcher";
+                settings = mkOption {
+                  type = types.submodule {
+                    options = {
+                      patches = lib.mkOption {
+                        type = types.listOf (types.either types.path types.package);
+                        default = [ ];
+                        example = literalExpression ''
+                          [
+                            (pkgs.fetchurl {
+                              name = "foo-module-init.patch";
+                              url = "https://github.com/NixOS/nixpkgs/compare/pull/123456/head~1...pull/123456/head.patch";
+                              hash = "";
+                            })
+                          ]
+                        '';
+                        description = ''
+                          A list of patches to apply to the nixpkgs source.
+                        '';
+                      };
+                    };
+                  };
+                  default = { };
+                };
+              };
+            };
+
+          dontCheckModule = {
+            # disable checking for "The option `services.xyz.enable' does not exist" and other errors
+            # this is a common error when using a module init patch
+            _module.check = false;
+          };
+
+          args' = {
+            system = null;
+            modules = args.modules ++ [
+              metadataModule
+              nixpkgsPatcherNixosModule
+            ];
+          }
+          // builtins.removeAttrs args [
+            "modules"
+            "nixpkgsPatcher"
+          ];
+
+          config = args.nixpkgsPatcher or { };
+          inputs =
+            config.inputs or args.specialArgs
+              or (die "Couldn't find your flake inputs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.inputs = inputs` or `specialArgs = inputs`.");
+          nixpkgs =
+            config.nixpkgs or inputs.nixpkgs
+              or (die "Couldn't find your base nixpkgs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.nixpkgs = inputs.nixpkgs` or name your main nixpkgs input `nixpkgs` and pass `specialArgs = inputs`.");
+          patchInputRegex = config.patchInputRegex or defaultPatchInputRegex;
+          patchesFromConfig = config.patches or (_: [ ]);
+
+          evalArgs = args' // {
+            modules = args'.modules ++ [ dontCheckModule ];
+          };
+          evaledModules = import "${nixpkgs}/nixos/lib/eval-config.nix" evalArgs;
+          system =
+            config.system or (
+              if args'.system != null then args'.system else evaledModules.config.nixpkgs.hostPlatform.system
+            );
+          pkgs = import nixpkgs { inherit system; };
+
+          moduleConfig = evaledModules.config.nixpkgs-patcher;
+          patchesFromModules = if moduleConfig.enable then moduleConfig.settings.patches else [ ];
+
+          patches =
+            (patchesFromFlakeInputs { inherit inputs patchInputRegex pkgs; })
+            ++ (patchesFromConfig pkgs)
+            ++ patchesFromModules;
+
+          enableTroubleshootingShell = config.enableTroubleshootingShell or true;
+
+          patchedNixpkgs = patchNixpkgsRaw {
+            inherit
+              nixpkgs
+              patches
+              enableTroubleshootingShell
+              pkgs
+              ;
+          };
+          finalNixpkgs = if patches == [ ] then nixpkgs else patchedNixpkgs;
+
+          nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix" args';
+        in
+        nixosSystem;
     in
     {
       lib = {
@@ -132,121 +248,7 @@
           in
           patchedNixpkgs;
 
-        nixosSystem =
-          args:
-          let
-            metadataModule = {
-              # this should be using `nixpkgsPatched` rather than `nixpkgs`
-              # but that will slow down every command that tries to look up the nixpkgs flake
-              # with the message 'copying "/nix/store/AAA..-patched" to the store'
-              config.nixpkgs.flake.source = toString nixpkgs;
-
-              config.system.nixos.versionSuffix = ".${nixpkgsVersion { inherit nixpkgs patches; }}";
-
-              config.system.nixos.revision = nixpkgs.rev or "dirty";
-            };
-
-            nixpkgsPatcherNixosModule =
-              { lib, ... }:
-
-              let
-                inherit (lib)
-                  mkOption
-                  mkEnableOption
-                  literalExpression
-                  types
-                  ;
-              in
-              {
-                options.nixpkgs-patcher = {
-                  enable = mkEnableOption "nixpkgs-patcher";
-                  settings = mkOption {
-                    type = types.submodule {
-                      options = {
-                        patches = lib.mkOption {
-                          type = types.listOf (types.either types.path types.package);
-                          default = [ ];
-                          example = literalExpression ''
-                            [
-                              (pkgs.fetchurl {
-                                name = "foo-module-init.patch";
-                                url = "https://github.com/NixOS/nixpkgs/compare/pull/123456/head~1...pull/123456/head.patch";
-                                hash = "";
-                              })
-                            ]
-                          '';
-                          description = ''
-                            A list of patches to apply to the nixpkgs source.
-                          '';
-                        };
-                      };
-                    };
-                    default = { };
-                  };
-                };
-              };
-
-            dontCheckModule = {
-              # disable checking for "The option `services.xyz.enable' does not exist" and other errors
-              # this is a common error when using a module init patch
-              _module.check = false;
-            };
-
-            args' = {
-              system = null;
-              modules = args.modules ++ [
-                metadataModule
-                nixpkgsPatcherNixosModule
-              ];
-            }
-            // builtins.removeAttrs args [
-              "modules"
-              "nixpkgsPatcher"
-            ];
-
-            config = args.nixpkgsPatcher or { };
-            inputs =
-              config.inputs or args.specialArgs
-                or (die "Couldn't find your flake inputs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.inputs = inputs` or `specialArgs = inputs`.");
-            nixpkgs =
-              config.nixpkgs or inputs.nixpkgs
-                or (die "Couldn't find your base nixpkgs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.nixpkgs = inputs.nixpkgs` or name your main nixpkgs input `nixpkgs` and pass `specialArgs = inputs`.");
-            patchInputRegex = config.patchInputRegex or defaultPatchInputRegex;
-            patchesFromConfig = config.patches or (_: [ ]);
-
-            evalArgs = args' // {
-              modules = args'.modules ++ [ dontCheckModule ];
-            };
-            evaledModules = import "${nixpkgs}/nixos/lib/eval-config.nix" evalArgs;
-            system =
-              config.system or (
-                if args'.system != null then args'.system else evaledModules.config.nixpkgs.hostPlatform.system
-              );
-            pkgs = import nixpkgs { inherit system; };
-
-            moduleConfig = evaledModules.config.nixpkgs-patcher;
-            patchesFromModules = if moduleConfig.enable then moduleConfig.settings.patches else [ ];
-
-            patches =
-              (patchesFromFlakeInputs { inherit inputs patchInputRegex pkgs; })
-              ++ (patchesFromConfig pkgs)
-              ++ patchesFromModules;
-
-            enableTroubleshootingShell = config.enableTroubleshootingShell or true;
-
-            patchedNixpkgs = patchNixpkgsRaw {
-              inherit
-                nixpkgs
-                patches
-                enableTroubleshootingShell
-                pkgs
-                ;
-            };
-            finalNixpkgs = if patches == [ ] then nixpkgs else patchedNixpkgs;
-
-            nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix" args';
-          in
-          nixosSystem;
+        inherit nixosSystem;
       };
     };
 }
