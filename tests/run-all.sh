259e66bc60ecaebfff1ca6e4375f7ec53039a25d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-06-12 01:32:51 +0200

    test: run on the current system

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nix
 set -x
+
+system="$(nix-instantiate --eval --raw -E 'builtins.currentSystem')"
+
 for testDir in */; do
   pushd $testDir
-  nix run github:NixOS/nixpkgs#nixVersions.stable -- build .#checks.x86_64-linux.tests
+  nix run github:NixOS/nixpkgs#nixVersions.stable -- build .#checks.$system.tests
   # TODO: fix "error: lock file contains mutable lock '{"path":"../..","type":"path"}'"
-  #nix run github:NixOS/nixpkgs#lixPackageSets.stable.lix -- build .#checks.x86_64-linux.tests
+  #nix run github:NixOS/nixpkgs#lixPackageSets.stable.lix -- build .#checks.$system.tests
   popd
 done
