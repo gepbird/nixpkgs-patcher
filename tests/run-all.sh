cd28aac38eb8ec2f1e1ffaae0d47381128d4ebd6
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-06-12 02:44:07 +0200

    test: skip trying to run checks if test is not for current system

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nix
-set -x
 
 system="$(nix-instantiate --eval --raw -E 'builtins.currentSystem')"
 
 for testDir in */; do
-  pushd $testDir
-  nix run github:NixOS/nixpkgs#nixVersions.stable -- build .#checks.$system.tests
-  # TODO: fix "error: lock file contains mutable lock '{"path":"../..","type":"path"}'"
-  #nix run github:NixOS/nixpkgs#lixPackageSets.stable.lix -- build .#checks.$system.tests
-  popd
+  pushd "$testDir" || exit 1
+
+  # Check if this test defines checks for the current system; skip if not
+  if [ "$(nix eval --impure --expr "(builtins.getFlake (toString ./.)).checks ? \"$system\"" 2>/dev/null)" = true ]; then
+    nix run github:NixOS/nixpkgs#nixVersions.stable -- build ".#checks.$system.tests"
+    # TODO: fix "error: lock file contains mutable lock '{"path":"../..","type":"path"}'"
+    #nix run github:NixOS/nixpkgs#lixPackageSets.stable.lix -- build ".#checks.$system.tests"
+    echo "Finished $testDir"
+  else
+    echo "Skipping $testDir (no checks for system '$system')"
+  fi
+
+  popd &> /dev/null || exit 1
 done
